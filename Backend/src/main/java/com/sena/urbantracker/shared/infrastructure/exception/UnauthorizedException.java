69a7dcf3e54281c216855cd9090b3dabb71de3b5
brayan-carvajal <chochosick8@gmail.com>
2025-11-02 14:27:36 -0500

    fix : reset branch

--- Backend/src/main/java/com/sena/urbantracker/shared/infrastructure/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.sena.urbantracker.shared.infrastructure.exception;
-
-public class UnauthorizedException extends RuntimeException {
-
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
--- Backend/src/main/java/com/sena/urbantracker/vehicles/infrastructure/repository/jpa/VehicleJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sena.urbantracker.vehicles.infrastructure.repository.jpa;
-
-import com.sena.urbantracker.vehicles.infrastructure.persistence.model.VehicleModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleJpaRepository extends JpaRepository<VehicleModel, Long> {
-
-    boolean existsByLicencePlate(String licencePlate);
-}
